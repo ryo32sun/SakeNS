e046d9c78a11fd59fe1f965e768995ab9bd2b983
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2022-08-09 09:08:52 +0000

    [add]コメント非同期

--- app/controllers/public/sake_comments_controller.rb
+++ app/controllers/public/sake_comments_controller.rb
@@ -2,18 +2,18 @@ class Public::SakeCommentsController < ApplicationController
   
   def create
     # binding.pry
-    sake_post = SakePost.find(params[:sake_post_id])
+    @sake_post = SakePost.find(params[:sake_post_id])
     # comment = current_customer.sake_comments.new(sake_comment_params)
     sake_comment = SakeComment.new(sake_comment_params)
     sake_comment.customer_id = current_customer.id
-    sake_comment.sake_post_id = sake_post.id
+    sake_comment.sake_post_id = @sake_post.id
     sake_comment.save
-    redirect_to sake_post_path(sake_post)
   end
   
   def destroy
-    SakeComment.find(params[:id]).destroy
-    redirect_to sake_post_path(params[:sake_post_id])
+    sake_comment = SakeComment.find(params[:id])
+    @sake_post = SakePost.find_by(id: sake_comment.sake_post_id)
+    sake_comment.destroy
   end
   
   private
