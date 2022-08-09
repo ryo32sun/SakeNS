83660d10503b80128f23b256fe33d9d1ab682a59
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2022-08-09 08:41:28 +0000

    [add] shop style いいね非同期

--- app/controllers/public/sake_favorites_controller.rb
+++ app/controllers/public/sake_favorites_controller.rb
@@ -4,17 +4,17 @@ class Public::SakeFavoritesController < ApplicationController
   end
   
   def create
-    sake_post = SakePost.find(params[:sake_post_id])
-    favorite = current_customer.sake_favorites.new(sake_post_id: sake_post.id)
+    @sake_post = SakePost.find(params[:sake_post_id])
+    @customer = @sake_post.customer
+    favorite = current_customer.sake_favorites.new(sake_post_id: @sake_post.id)
     favorite.save
-    redirect_to sake_post_path(sake_post)
   end
   
   def destroy
-    sake_post = SakePost.find(params[:sake_post_id])
-    favorite = current_customer.sake_favorites.find_by(sake_post_id: sake_post.id)
+    @sake_post = SakePost.find(params[:sake_post_id])
+    @customer = @sake_post.customer
+    favorite = current_customer.sake_favorites.find_by(sake_post_id: @sake_post.id)
     favorite.destroy
-    redirect_to sake_post_path(sake_post)
   end
 
 end
